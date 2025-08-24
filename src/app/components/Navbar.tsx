ddbcd57ef5fffd7bbf89133f1ff03dbe350d2247
FabianHenrick <fabianhenrick@gmail.com>
2025-08-24 16:48:04 -0300

    nav menu styled and functional

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,21 +1,77 @@
 import Image from 'next/image'
 import React from 'react'
 import Link from 'next/link'
-import Home from '../page'
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
 
 export default function Navbar() {
   return (
-    <div className='flex justify-between border-b-2 border-black margin-0'>
-      <Image src="/logo.png" alt="Logo" width={100} height={50} />
-      <ul className='flex gap-4 p-4 content-center justify-center'>
-        <li><Link href="/">Home</Link></li>
-        <li><Link href="/about">Sobre</Link></li>
-        <li><Link href="/teams">Times</Link></li>
-        <li><Link href="/championships">Campeonatos</Link></li>
-        <li><Link href="/shop">Shop</Link></li>
-        <li><Link href="/contacts">Contatos</Link></li>
-        <li><Link href="/partner">Seja um sócio sofredor</Link></li>
-      </ul>
+  <div className="flex justify-between items-center ">
+  <Image
+    src="/logo-susurradores.png"
+    alt="Logo"
+    width={190}
+    height={190}
+    className="absolute"
+  />
+
+  <div className="flex flex-col s w-full ">
+    {/* Menu de cima */}
+    <div className="bg-black text-white  rounded-lg px-4 py-2">
+      <NavigationMenu className=" ml-auto">
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>
+              <Link href="https://www.instagram.com/sussurradores/reels/?hl=nb">Instagram</Link>
+            </NavigationMenuTrigger>
+            <NavigationMenuTrigger>
+              <Link href="/about">Twitter</Link>
+            </NavigationMenuTrigger>
+            <NavigationMenuTrigger>
+              <Link href="/shop">Facebook</Link>
+            </NavigationMenuTrigger>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
     </div>
+
+    {/* Menu de baixo */}
+    <div className="bg-white text-black rounded-lg px-4 py-2 mt-2 ml-auto">
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>
+              <Link href="/">Home</Link>
+            </NavigationMenuTrigger>
+            <NavigationMenuTrigger>
+              <Link href="/about">Sobre</Link>
+            </NavigationMenuTrigger>
+            <NavigationMenuTrigger>
+              <Link href="/shop">Shop</Link>
+            </NavigationMenuTrigger>
+            <NavigationMenuTrigger>
+              <Link href="/championships">Campeonatos</Link>
+            </NavigationMenuTrigger>
+            <NavigationMenuTrigger>
+              <Link href="/teams">Times</Link>
+            </NavigationMenuTrigger>
+            <NavigationMenuTrigger>
+              <Link href="/partner">Sócio Sofredor</Link>
+            </NavigationMenuTrigger>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
+  </div>
+</div>
+  
   )
 }
